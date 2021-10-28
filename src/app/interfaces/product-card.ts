cb2c03521c4bc84ae4d7fb516abaea4fa711a357
Andrii  Matseliukh <amatseliukh@reenbit.com>
2021-10-28 11:09:38 +0300

    firebase installed

---
+++ src/app/interfaces/product-card.ts
@@ -0,0 +1,10 @@
+export interface ProductCard {
+  productTitle: string;
+  description: string;
+  freshness: string;
+  farm: string;
+  delivery: string;
+  stock: number;
+  priceUsual: number;
+  pricePromotional: number;
+}
