734f7bdbb721f879473c822af45a1ea9566dfd9a
Andrii  Matseliukh <amatseliukh@reenbit.com>
2021-10-30 19:52:31 +0300

    bugs in styles for pagination have been fixed

--- src/app/components/all-products-components/all-products-content/all-products-content.component.ts
+++ src/app/components/all-products-components/all-products-content/all-products-content.component.ts
@@ -4,7 +4,7 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-all-products-content',
   templateUrl: './all-products-content.component.html',
-  styleUrls: ['./all-products-content.component.css'],
+  styleUrls: ['./all-products-content.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AllProductsContentComponent implements OnInit {
