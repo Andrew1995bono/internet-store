8c484649f284424da22c6eb0216e3fe52349965f
Andrii  Matseliukh <amatseliukh@reenbit.com>
2021-11-11 15:33:57 +0200

    mixins have been created for different devices display sizes and added to the header component

--- src/app/components/homepage-components/navbar/select-field/select-field.component.ts
+++ src/app/components/homepage-components/navbar/select-field/select-field.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-select-field',
   templateUrl: './select-field.component.html',
-  styleUrls: ['./select-field.component.css']
+  styleUrls: ['./select-field.component.scss']
 })
 
 export class SelectFieldComponent implements OnInit {
