dfc515b1778d8045dc62bbca326e2f44e52a0459
Andrii  Matseliukh <amatseliukh@reenbit.com>
2021-11-19 20:31:46 +0200

    dynamic data has been passed to more products block, added styling according to the mockup

--- src/app/components/product-detail-components/product-detail.component.ts
+++ src/app/components/product-detail-components/product-detail.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 import { ProductCard } from '../../interfaces/product-card';
 import { FiltersService } from '../../services/all-products/filters/filters.service';
 import { ProductDetailService } from '../../services/product-detail/product-detail.service';
@@ -16,7 +14,7 @@ export class ProductDetailComponent implements OnInit {
 
   public starArr: string[] = ['', '', '', '', ''];
   public product = {} as ProductCard;
-  private unsubscribe$ = new Subject();
+  public products: ProductCard[] = [];
 
   constructor(
     private productDetailService: ProductDetailService,
@@ -26,29 +24,17 @@ export class ProductDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.setRating(this.product);
-    this.productDetailService.individualProduct.pipe(
-      takeUntil(this.unsubscribe$)
-    ).subscribe(data => {
-      this.product = data;
-    });
+    this.products = this.filtersService.products.value;
     this.route.params.subscribe(data => {
       this.product = this.filtersService.products.value.find(item => item.itemID === data.id) || new ProductCard();
+      this.products = this.filtersService.products.value;
     });
   }
-
 
   private setRating(product: ProductCard): void {
     this.starArr.fill('../../assets/rate-star-filled.png', 0, (product.rating));
     this.starArr.fill('../../assets/rate-star.png', product.rating, 5);
   }
 
-  private setIndividualProduct(): void {
-    this.productDetailService.individualProduct.subscribe(data => this.product = data);
-  }
-
-  public ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
 
 }
